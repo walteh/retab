9ad5c80513806270342ccc260bb6dd7a051fe65a
walter <iamwalteh@me.com>
2023-06-11 16:05:02 -0500

    oops - fix bug just introduced

--- tftab.sh
+++ tftab.sh
@@ -4,7 +4,7 @@ file="$1"
 
 if command -v terraform >/dev/null 2>&1; then
 	temp_file=$(mktemp)
-	"$1" fmt "$file" && sed -e'':a'' -e's/^\\(\\t*\\)  /\\1\\t/;ta' "$file" >"$temp_file" && mv "$temp_file" "$file"
+	terraform fmt "$file" && sed -e'':a'' -e's/^\\(\\t*\\)  /\\1\\t/;ta' "$file" >"$temp_file" && mv "$temp_file" "$file"
 	exit 0
 else
 	echo "terraform not found"
