ee810aee6b0107eaf8892ff29ee3074f44c31e3f
walter <iamwalteh@me.com>
2023-06-11 16:20:01 -0500

    no terraform check

--- tftab.sh
+++ tftab.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
 file="$1"
-
-if command -v terraform >/dev/null 2>&1; then
-	temp_file=$file-notab
-	terraform fmt "$file" && sed -e'':a'' -e's/^\\(\\t*\\)  /\\1\\t/;ta' "$file" >"$temp_file" && mv "$temp_file" "$file"
-	exit 0
-else
-	echo "terraform not found"
-	exit 1
-fi
+temp_file=$file-notab
+terraform fmt "$file" && sed -e'':a'' -e's/^\\(\\t*\\)  /\\1\\t/;ta' "$file" >"$temp_file" && mv "$temp_file" "$file"
